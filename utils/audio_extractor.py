b28b74451cf6a87590c2b451620527d6662bdcdc
elihusela <elihusela.es@gmail.com>
2025-07-02 12:50:47 +0300

    handle wav files

--- utils/audio_extractor.py
+++ utils/audio_extractor.py
@@ -7,6 +7,8 @@ class AudioExtractor:
         self._audio_result_path = audio_result_path
 
     def extract_audio_from_video(self) -> str:
+        if ".wav" in self._vid_path:
+            return self._vid_path
         video_clip = VideoFileClip(self._vid_path)
         audio = video_clip.audio
         audio.write_audiofile(self._audio_result_path)
